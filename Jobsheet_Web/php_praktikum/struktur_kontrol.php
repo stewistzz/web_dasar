52e57e19121ba910849d231230b9a20fbd212f9f
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-18 15:54:43 +0700

    jumlahBuah tambahan

--- Jobsheet_Web/php_praktikum/struktur_kontrol.php
+++ Jobsheet_Web/php_praktikum/struktur_kontrol.php
@@ -24,4 +24,19 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 echo"Atel tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer";
 
+
+// tambahan 3
+echo"<br><br>";
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i=1; $i <= $jumlahLahan ; $i++) { 
+    // jumlah buah
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo"Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>
