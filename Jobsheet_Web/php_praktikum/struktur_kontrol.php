b13972d9b512f6f5e13913b7146c63db66fa447c
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-18 16:08:27 +0700

    tambahan 5 if foreach

--- Jobsheet_Web/php_praktikum/struktur_kontrol.php
+++ Jobsheet_Web/php_praktikum/struktur_kontrol.php
@@ -52,4 +52,18 @@ foreach ($skorUjian as $skor) {
 }
 
 echo"Total skor ujian adalah: $totalSkor";
+
+// tambahan 5
+// foreach
+echo"<br><br>";
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo"Nilai: $nilai (Tidak luluis) <br>";
+        continue;
+    }
+    echo"Nilai: $nilai (Lulus) <br>";
+}
+
 ?>
