951d39dfcff7e8f64f31bb20b1e8ed03f75e9177
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 15:01:57 +0700

    js05_praktikum9

---
+++ Jobsheet_Web/js05_PHP02/prak9/entities.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Entities HTML</title>
+</head>
+<body>
+
+    <p>lt&#39; time to reat a HTML5 book.</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp; Keuntungan dari menggunakan nama entities: Sebuah nama entitas mudah diingat. Keruigian dari menggunakan nama entities: Browser mungkin tidak mendukung semua nama entitas, tetapi dukungan untuk nomor lebih baik.</p>
+
+    <p>&nbsp;&nbsp; Keuntungan dari menggunakan nama entities: Sebuah nama entitas mudah diingat. Keruigian dari menggunakan nama entities: Browser mungkin tidak mendukung semua nama entitas, tetapi dukungan untuk nomor lebih baik.</p>
+    <p>&#169; 2023 jti.com</p>
+</body>
+</html>
