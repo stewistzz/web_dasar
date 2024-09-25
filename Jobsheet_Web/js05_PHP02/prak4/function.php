1489c983bec10639d06853dc454c89434a457dba
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 10:02:58 +0700

    js05_praktikum4_fungsi_default_value

--- Jobsheet_Web/js05_PHP02/prak4/function.php
+++ Jobsheet_Web/js05_PHP02/prak4/function.php
@@ -12,14 +12,13 @@
     perkenalan();
     echo"<br><br>";
 
-    // modifikasi dengan parameter
+    /* modifikasi fungsi dengan parameter */
     function perkenalanParameter($nama, $salam)
     {
         echo $salam;
         echo "Perkenalkan, nama saya" . $nama . "</br>"; // php dapat menyimpan tag html didalam echo
          echo "Senang berkenalan dengan anda<br>";
     }
-
     // pemanggilan fungsi yang dibuat dengan value pada parameter
     perkenalanParameter("Dimas Setyo", "Hallo");
 
@@ -27,7 +26,25 @@
 
     $saya = "Nugroho";
     $ucapanSalam = "Selamat Pagi";
-
     // pemanggilan fungsi dengan value pada variabel
     perkenalanParameter($saya, $ucapanSalam);
+    echo"<br><br>";
+
+    /* modifikasi fungsi dengan parameter add nilai default */
+    function perkenalanDefault($nama, $salam ="Assalamualaikum")
+    {
+        echo $salam;
+        echo " Perkenalkan, nama saya" . $nama . "</br>";
+         echo "Senang berkenalan dengan anda<br>";
+    }
+    // memanggil fungsi yang sudah dibuat
+    perkenalanDefault("Dimas Setyo", "Hallo");
+    echo"<hr>";
+
+    // memanggil fungsi dengan parameter variabel
+    $sayaNew = "Setyo";
+    $ucapanSalamNew = "Selamat pagi";
+    // call function
+    perkenalanDefault($sayaNew);
+
 ?>
