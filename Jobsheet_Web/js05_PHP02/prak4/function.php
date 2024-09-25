94e7bf6306cecfcd2f7b020bd042fb4ab47ad93b
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 09:50:30 +0700

    js05_praktikum4_fungsi_parameter

--- Jobsheet_Web/js05_PHP02/prak4/function.php
+++ Jobsheet_Web/js05_PHP02/prak4/function.php
@@ -10,4 +10,24 @@
     // calling function
     perkenalan();
     perkenalan();
+    echo"<br><br>";
+
+    // modifikasi dengan parameter
+    function perkenalanParameter($nama, $salam)
+    {
+        echo $salam;
+        echo "Perkenalkan, nama saya" . $nama . "</br>"; // php dapat menyimpan tag html didalam echo
+         echo "Senang berkenalan dengan anda<br>";
+    }
+
+    // pemanggilan fungsi yang dibuat dengan value pada parameter
+    perkenalanParameter("Dimas Setyo", "Hallo");
+
+    echo"<hr>";
+
+    $saya = "Nugroho";
+    $ucapanSalam = "Selamat Pagi";
+
+    // pemanggilan fungsi dengan value pada variabel
+    perkenalanParameter($saya, $ucapanSalam);
 ?>
