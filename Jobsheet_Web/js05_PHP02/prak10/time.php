a98be155696b6ea3f19e23e5e8621c406fb9a8a5
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 15:30:18 +0700

    js-05_praktikum10_time

---
+++ Jobsheet_Web/js05_PHP02/prak10/time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php
+    date_default_timezone_set("asia/jakarta");
+    echo date("h:i:sa")
+    ?>
+</body>
+
+</html>
