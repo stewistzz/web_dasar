0b3ae147623cd69624e0daf096c4453957caef7d
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 14:04:02 +0700

    js05_praktikum7_bagian2_string2

---
+++ Jobsheet_Web/js05_PHP02/prak7/string2.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo"Baris\nbaru <br>";
+echo'Baris\nbaru <br>';
+echo"Hallo\rDunia <br>";
+echo'Hallo\rDunia <br>';
+
+echo"<pre>Halo\tDunia!</pre>";
+echo'<pre>Halo\tDunia!</pre>';
+
+echo"Katakankalh \"Tidak pada narkoba\" <br>";
+echo'Katakankalh \'Tidak pada narkoba\' <br>';
+
+?>
