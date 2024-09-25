2b6128999a53754ab8cc61d73cc20d57dc9635ae
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 14:26:05 +0700

    js05_praktikum7_bagian3

--- Jobsheet_Web/js05_PHP02/prak7/string3.php
+++ Jobsheet_Web/js05_PHP02/prak7/string3.php
@@ -1,4 +1,19 @@
 <?php 
 $pesan = "Saya arek Tulungagung";
 echo strrev($pesan) . "<br>";
+
+// bagian 2
+$pesan = "Saya arek Tulungagung";
+// ubah variabel $pesan menjadi array dengan perintah explode
+
+$pesanPerkata = explode(" ", $pesan);
+// ubah setiap kata dalam array menjadi kebalikannya
+
+$pesanPerkata = array_map(fn($pesan) => strrev($pesan), $pesanPerkata);
+// gabungkan kembali array menjadi string
+
+$pesan = implode(" ", $pesanPerkata);
+
+echo $pesan . "<br>";
+
 ?>
