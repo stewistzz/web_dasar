49ba72b764de5eb8e0d358906b9354897eebaa93
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 09:38:55 +0700

    js05_praktikum4_fungsi

---
+++ Jobsheet_Web/js05_PHP02/prak4/function.php
@@ -0,0 +1,13 @@
+<?php 
+    // belajar membuat function
+    function perkenalan()
+    {
+        echo "Assalamualaikum, ";
+        echo "Perkenalkan, nama saya Dimas Setyo</br>"; // php dapat menyimpan tag html didalam echo
+         echo "Senang berkenalan dengan anda<br>";
+    }
+
+    // calling function
+    perkenalan();
+    perkenalan();
+?>
