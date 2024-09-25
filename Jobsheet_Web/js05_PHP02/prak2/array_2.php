a8122d613a11f684270bef2f8f9b50c5f70af813
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-09-25 07:04:50 +0700

    js05_praktikum2

---
+++ Jobsheet_Web/js05_PHP02/prak2/array_2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: wheat;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+    echo "Nama : {$Dosen['nama']}<br>";
+    echo "Domisili : {$Dosen['domisili']}<br>";
+    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']}<br>"
+    ?>
+</body>
+
+</html>
