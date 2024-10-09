2b3c9ef64e4e0487ad6ad23a4221023a6c9e45b6
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-10-09 12:09:23 +0700

    jobsheet7_empty_bagian1

---
+++ Jobsheet_Web/PHP_FormProccessing/FunctionEmpty/empty.php
@@ -0,0 +1,8 @@
+<?php   
+    $myArray = array(); //empty array
+    if (empty($myArray)) {
+        echo"Array tidak terdefinisi atau kosong.";
+    } else {
+        echo"Array terdefinisi dan tidak kosong";
+    }
+?>
