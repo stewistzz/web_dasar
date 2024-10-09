a83b7f1200d445de8548aeac4a6e59080c0245d0
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-10-09 12:27:07 +0700

    jobsheet7_formInputPHP_bagian1

---
+++ Jobsheet_Web/PHP_FormProccessing/FormInput/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Input PHP</title>
+</head>
+<body>
+    <h2>Form Input PHP</h2>
+    <form action="proses_form.php" method="post">
+        <label for="nama">Nama : </label>
+        <input type="text" name="nama" id="nama">
+
+        <label for="email">Email : </label>
+        <input type="text" name="email" id="email">
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
