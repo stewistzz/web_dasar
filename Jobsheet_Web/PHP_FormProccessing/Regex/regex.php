6561ad7385e9ab5b31bbb0dee0edc49298a2cebc
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-10-09 14:50:21 +0700

    jobsheet7_regex_bagian3

--- Jobsheet_Web/PHP_FormProccessing/Regex/regex.php
+++ Jobsheet_Web/PHP_FormProccessing/Regex/regex.php
@@ -9,4 +9,31 @@
         echo"Tidak ada huruf kecil!";
     }
     
+    echo"<br><hr>";
+    $pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+
+    if (preg_match($pattern, $text,$matches)) {
+        echo"Cocokkan : " . $matches[0];
+    } else {
+        echo"Tidak ada yang cocok";
+    }
+
+    echo"<br><hr>";
+    $pattern = '/apple/';
+    $replacement = 'banana';
+    $text = 'I like apple pie. ';
+    $new_text = preg_replace($pattern, $replacement, $text);
+    echo $new_text;
+
+    echo"<br><hr>";
+    $pattern = '/go*d/'; // mencari kecocokan kata god, good, goopd, tergantung jumlah o nya sebanyak kali
+    $text = 'god is good';
+
+    if (preg_match($pattern, $text, $matches)) {
+        echo"Cocokkan : " . $matches[0];
+    } else {
+        echo"Tidak ada yang cocok!";
+    }
+    
 ?>
