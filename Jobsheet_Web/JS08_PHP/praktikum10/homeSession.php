238bbe3a4cafe106245764d251405c09258b169a
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-10-16 16:39:07 +0700

    JS08_praktikum10

---
+++ Jobsheet_Web/JS08_PHP/praktikum10/homeSession.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <?php 
+        session_start();
+        if ($_SESSION['status'] == 'login') {
+            echo"Selamat datang " . $_SESSION['username'];
+        }
+    ?>
+    <br><a href="sessionLogout.php">logout</a>
+</body>
+</html>
