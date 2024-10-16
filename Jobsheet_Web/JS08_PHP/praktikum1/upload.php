29d0e7e5be653cbc5f13aee173fb3e82394cded3
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-10-16 11:54:36 +0700

    JS08_praktikum1_pertanyaan3

--- Jobsheet_Web/JS08_PHP/praktikum1/upload.php
+++ Jobsheet_Web/JS08_PHP/praktikum1/upload.php
@@ -4,12 +4,23 @@
         $targetdir = "uploads/"; // destinasi directory
         $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
 
-        // kondisi
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah";
+        // ubahan1
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxsize = 5*1024*1024;
+
+        // new condition
+        if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+            // kondisi
+            if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+                echo "File berhasil diunggah";
+            } else {
+                echo "File gagal diunggah";
+            }
         } else {
-            echo "File gagal diunggah";
-        }
+            echo"File tidak valid atau melebihi ukuran maksumum yang diizinkan";        }
+        
     }
 
 ?>
