c93a2fa4e21d8ba4e4588830280caa4107ee4378
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-11-13 12:49:19 +0700

    JS12_OOP_PHP_Praktikum1_Basic_soal1.5

--- Jobsheet_Web/OOP_PHP/Basic_OOP/oop.php
+++ Jobsheet_Web/OOP_PHP/Basic_OOP/oop.php
@@ -135,4 +135,42 @@ $car->setColor("Red");
 
 echo"Updated Color: " . $car->getColor() . "<br>";
 
+
+echo"<hr>";
+// praktikum 5 -- abstraction
+abstract class SShape {
+    abstract public function calculateArea();
+}
+
+class CCircle extends SShape{
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function calculateArea() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+class RRectangle extends SShape{
+    private $width;
+    private $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function calculateArea() {
+        return $this->width * $this->height;
+    }
+}
+$ccircle = new CCircle(5);
+$rrectangle = new RRectangle(4, 6);
+
+echo"Area of Circle : " . $ccircle->calculateArea() . "<br>";
+echo"Area of Rectangle : " . $rrectangle->calculateArea() . "<br>";
+
+
 ?>
